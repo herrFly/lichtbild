070a9bd944f35948882149bba3aecfb82591ae9a
herrFly <martenkirschenhofer@gmail.com>
2021-10-08 13:35:27 +0200

    ueberMich angepasst

--- src/pages/ueberMich.js
+++ src/pages/ueberMich.js
@@ -1,58 +1,65 @@
-import React from "react"
-import { Helmet } from "react-helmet"
-import { Link } from "gatsby"
+import React from "react";
+import { Helmet } from "react-helmet";
+import { Link } from "gatsby";
 
-import "@fontsource/lato/300.css"
+import "@fontsource/lato/300.css";
 
-import { slide as Menu } from "react-burger-menu"
+import { slide as Menu } from "react-burger-menu";
 
-import "../../static/assets/css/main.css"
-import "../../static/assets/css/noscript.css"
-import "../../static/assets/css/fontawesome-all.min.css"
+import "../../static/assets/css/main.css";
+import "../../static/assets/css/noscript.css";
+import "../../static/assets/css/fontawesome-all.min.css";
 
-import MyForm from "./kontakt"
-import MyFooter from "../components/footer"
+import MyForm from "./kontakt";
+import MyFooter from "../components/footer";
 
-import ubermich from "../../static/img/PROFIL/UeberMich.webp"
+import ubermich from "../../static/img/PROFIL/UeberMich.webp";
 
 const ueberMich = () => (
   <>
-       <Menu>
-       <Link to="/" className="menu-item">
-          Startseite
-        </Link>
-        <br />
-        {/* <Link to="/home/" className="menu-item">
+    <Menu>
+      <Link to="/" className="menu-item">
+        Startseite
+      </Link>
+      <br />
+      {/* <Link to="/home/" className="menu-item">
           Terminanfrage
         </Link> */}
-        <Link to="/ueberMich" className="menu-item">
-          Über Mich
-        </Link>
-        <Link to="/familie" className="menu-item">
-          Familie
-        </Link>
-        <Link to="/hochzeiten" className="menu-item">
-          Hochzeiten
-        </Link>
-        <Link to="/babybauch" className="menu-item">
-          Babybauch
-        </Link>
-        <Link to="/hunde" className="menu-item">
-          Hunde
-        </Link>
-        <br />
-        <Link to="/faq" className="menu-item">
-          FAQ
-        </Link>
+      <Link to="/ueberMich" className="menu-item">
+        Über Mich
+      </Link>
+      <Link to="/familie" className="menu-item">
+        Familie
+      </Link>
+      <Link to="/hochzeiten" className="menu-item">
+        Hochzeiten
+      </Link>
+      <Link to="/babybauch" className="menu-item">
+        Babybauch
+      </Link>
+      <Link to="/hunde" className="menu-item">
+        Hunde
+      </Link>
+      <br />
+      <Link to="/faq" className="menu-item">
+        FAQ
+      </Link>
 
-        <a href="https://www.instagram.com/lichtbild_enthusiastin/" target="_blank" rel="noopener noreferrer">
-          <i className="fab fa-instagram" id="insta-logo"></i>
-        </a>
-        <a href="https://api.whatsapp.com/send?phone=4903091579337" target="_blank" rel="noopener noreferrer">
-          <i className="fab fa-whatsapp" id="whatsapp-logo"></i>
-        </a>
-        
-      </Menu>
+      <a
+        href="https://www.instagram.com/lichtbild_enthusiastin/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i className="fab fa-instagram" id="insta-logo"></i>
+      </a>
+      <a
+        href="https://api.whatsapp.com/send?phone=4903091579337"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i className="fab fa-whatsapp" id="whatsapp-logo"></i>
+      </a>
+    </Menu>
     <Helmet>
       <title>Lichtbildenthusiastin - Deine Fotografin</title>
     </Helmet>
@@ -60,15 +67,14 @@ const ueberMich = () => (
     <main>
       <div id="wrapper" className="divided">
         <section className="wrapper style1 align-center">
-          <img id="uebermichPic" src={ubermich}></img> 
-
+          <img id="uebermichPic" src={ubermich}></img>
         </section>
-        <section/>
-        <MyForm />
-        <MyFooter />
-
+        <section className="wrapper style1 align-center">
+          <MyForm />
+          <MyFooter />
+        </section>
       </div>
     </main>
   </>
-)
-export default ueberMich
+);
+export default ueberMich;
